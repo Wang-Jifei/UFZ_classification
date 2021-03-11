3539365af2b5932567978e5bc4df96e9dad18481
Wang-Jifei <78724371+Wang-Jifei@users.noreply.github.com>
2021-03-12 03:20:25 +0800

    1

--- tf_idf.py
+++ tf_idf.py
@@ -6,7 +6,7 @@ import numpy as np
 Calculate tf-idf
 """
 # 该类会将文本中的词语转换为词频矩阵，矩阵元素a[i][j] 表示j词在i类文本下的词频
-vectorizer = CountVectorizer(max_features=10)
+vectorizer = CountVectorizer(max_features=100)
 input_dir=r'Feature vector/word_frequency_file.txt'
 output_dir= r'Feature vector/word_tfidf.txt'
 data_mat = np.loadtxt(input_dir)
